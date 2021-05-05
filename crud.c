a6112b55541dcc6c4d7b751c795d98e74b6ee594
KimMinChae <alsco4321@gmail.com>
2021-05-05 19:41:02 +0900

    edit u

--- crud.c
+++ crud.c
@@ -32,7 +32,26 @@ void listClothes(Clothes *c, int count){
 }
 
 int updateClothes(Clothes *c){
+    printf("\n");
+    printf("제품명? ");
+    scanf("%[^\n]s",c->name);
 
+    printf("가격? ");
+    scanf("%d",&c->price);
+
+    printf("사이즈 (S, M, L, F(Free)? ");
+    scanf("%c",&c->size);
+
+    printf("리뷰개수? ");
+    scanf("%d",&c->review);
+
+    printf("별점개수? ");
+    scanf("%d",&c->num_stars);
+
+    //linked?
+
+    printf("==> 추가됨\n"); 
+    return 1;
 }
 
 int deleteClothes(Clothes *c){
