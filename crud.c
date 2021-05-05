2991e6c903803356cfa75cbf4fb241bc0b0d471b
KimMinChae <alsco4321@gmail.com>
2021-05-05 19:26:23 +0900

    make c

--- crud.c
+++ crud.c
@@ -5,15 +5,20 @@ int createClothes(Clothes *c){
     printf("제품명? ");
     scanf("%[^\n]s",c->name);
 
+    printf("가격? ");
+    scanf("%d",&c->price);
+
     printf("사이즈 (S, M, L, F(Free)? ");
     scanf("%c",&c->size);
+
+    printf("리뷰개수? ");
 
     printf("==> 추가됨\n"); 
     return 1;
 }
 
 void readClothes(Clothes *c){
-
+    printf("%-15s %3dg %4d\n",c->name,c->size,c->price);
 }
 
 void listClothes(Clothes *c, int count){
