a4e29250c8f8aa2ce87648b79fe38e490f490c08
alsco1234 <22000080@handong.edu>
2021-05-07 11:56:12 +0900

    testNameEmail

--- manager.c
+++ manager.c
@@ -2,7 +2,7 @@
 
 void saveData(Clothes *c, int count); //데이터 저장
 int loadData(Clothes *c); //데이터 읽어오기
-
+///
 int searchName(Clothes *c, int count);
 int searchPrice(Clothes *c, int count);///
 int searchSize(Clothes *c, int count);
