92a66075d042d18f73008e92f9d3deb3dec4098c
22000217 <80326384+22000217@users.noreply.github.com>
2021-05-10 18:17:43 +0900

    Modified changes

--- crud_cpp.h
+++ crud_cpp.h
@@ -8,22 +8,22 @@ class Clothes {
 private:
     string name;        //제품명
     int price;          //가격
-    char size;          //사이즈(S,M,L,F(free))
+    string size;          //사이즈(S,M,L,F(free))
     int review;         //리뷰 개수
     int numStars;       //별점 개수
 public:
     Clothes* link;      //다음 node를 가리키는 포인터
     //setter
-    void set_data(string n, int p, char s, int r, int ns);
+    void set_data(string n, int p, string s, int r, int ns);
     void set_name(string n);
     void set_price(int p);
-    void set_size(char s);
+    void set_size(string s);
     void set_review(int r);
     void set_numStars(int ns);
     //getter
     string get_name();
     int get_price();
-    char get_size();
+    string get_size();
     int get_review();
     int get_numStars();
     Clothes* get_link();
@@ -40,10 +40,6 @@ public:
     Clothes get_product(string p_name);
     int remove_product(string p_name);
 };
-
-
-
-Clothes clist[100]; //포인터 배열 변수 선언
 
 Clothes createClothes();
 
